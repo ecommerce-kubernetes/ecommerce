08397db727373c98585f6e92e6f6c51ec4e98745
Yu Ho Yeon <zmfmsh46@gmail.com>
2025-03-23 14:40:28 +0900

    2025-03-23

---
+++ user-service/src/main/java/com/example/userservice/dto/UserDto.java
@@ -0,0 +1,13 @@
+package com.example.userservice.dto;
+
+import java.util.Date;
+
+public class UserDto {
+    private String email;
+    private String name;
+    private String pwd;
+    private String userId;
+    private Date createAt;
+    private String encryptedPwd;
+
+}
