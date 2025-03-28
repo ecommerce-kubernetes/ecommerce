f73704e64fc15ea98c15579cd01a4b07da28d66b
minsik2434 <minsik2434@gmail.com>
2025-03-28 16:46:32 +0900

    Fix: validation 수정

--- product-service/src/main/java/com/example/product_service/dto/request/ProductRequestDto.java
+++ product-service/src/main/java/com/example/product_service/dto/request/ProductRequestDto.java
@@ -18,7 +18,7 @@ public class ProductRequestDto {
     private String name;
     @NotBlank(message = "Product description is required")
     private String description;
-    @Min(value = 0, message = "Product price must not be less than 0") @Max(value = 1000000, message = "Product price must not be greater than 10,000,000")
+    @Min(value = 0, message = "Product price must not be less than 0") @Max(value = 10000000, message = "Product price must not be greater than 10,000,000")
     private int price;
     @Min(value = 0, message = "Product stockQuantity must not be less than 0") @Max(value = 100, message = "Product stockQuantity must not be greater than 100")
     private int stockQuantity;
