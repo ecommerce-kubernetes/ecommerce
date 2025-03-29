5447bdc276794cf62cf88ad8327b2f351e28f554
minsik2434 <minsik2434@gmail.com>
2025-03-29 15:54:04 +0900

    Feat: config-service 설정

---
+++ order-service/src/main/java/com/example/order_service/controller/OrderController.java
@@ -0,0 +1,16 @@
+package com.example.order_service.controller;
+
+import com.example.order_service.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+}
