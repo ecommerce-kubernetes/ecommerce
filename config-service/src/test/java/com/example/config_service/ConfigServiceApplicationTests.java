ea258399b90c411d1a9066212a92ded42674b479
Yu Ho Yeon <zmfmsh46@gmail.com>
2025-04-26 18:39:37 +0900

    2025-04-26

--- config-service/src/test/java/com/example/config_service/ConfigServiceApplicationTests.java
+++ config-service/src/test/java/com/example/config_service/ConfigServiceApplicationTests.java
@@ -1,15 +1,7 @@
 package com.example.config_service;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
 
-
-@SpringBootTest
-@ImportAutoConfiguration(exclude = {
-		org.springframework.cloud.bootstrap.encrypt.EncryptionBootstrapConfiguration.class,
-		org.springframework.cloud.config.server.config.RsaEncryptionAutoConfiguration.class
-})
 class ConfigServiceApplicationTests {
 
 	@Test
