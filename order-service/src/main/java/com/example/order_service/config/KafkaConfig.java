7189393c8d60b93f58664aec23272951e4e8fe27
minsik2434 <minsik2434@gmail.com>
2025-05-02 19:01:46 +0900

    Feat: Order-service 카프카 설정

--- order-service/src/main/java/com/example/order_service/config/KafkaConfig.java
+++ order-service/src/main/java/com/example/order_service/config/KafkaConfig.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.*;
@@ -16,7 +17,8 @@ import java.util.Map;
 @Configuration
 public class KafkaConfig {
 
-    private static final String BOOTSTRAP_SERVERS = "localhost:9092";
+    @Value("${kafka.bootstrap_servers}")
+    private String BOOTSTRAP_SERVERS;
     private static final String GROUP_ID = "orders";
 
     @Bean
