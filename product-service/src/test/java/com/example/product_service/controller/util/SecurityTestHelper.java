dbd9c82731d34dedaf5d6b8fdd045aacced2f479
minsik2434 <minsik2434@gmail.com>
2025-08-04 18:42:40 +0900

    Feat : SecurityTestHelper
    보안 테스트 helper 구현

---
+++ product-service/src/test/java/com/example/product_service/controller/util/SecurityTestHelper.java
@@ -0,0 +1,34 @@
+package com.example.product_service.controller.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public final class SecurityTestHelper {
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    public static String toJson(Object o) throws JsonProcessingException {
+        return mapper.writeValueAsString(o);
+    }
+
+    public static void verifyNoPermissionResponse(ResultActions perform, String path) throws Exception {
+        verityErrorResponse(perform, status().isForbidden(), "Forbidden", "Access Denied", path);
+    }
+    public static void verifyUnauthorizedResponse(ResultActions perform, String path) throws Exception {
+        verityErrorResponse(perform, status().isUnauthorized(), "UnAuthorized", "Invalid Header", path);
+    }
+
+    private static void verityErrorResponse(ResultActions perform, ResultMatcher status, String error, String message, String path) throws Exception {
+        perform
+                .andExpect(status)
+                .andExpect(jsonPath("$.error").value(error))
+                .andExpect(jsonPath("$.message").value(message))
+                .andExpect(jsonPath("$.timestamp").exists())
+                .andExpect(jsonPath("$.path").value(path));
+    }
+}
