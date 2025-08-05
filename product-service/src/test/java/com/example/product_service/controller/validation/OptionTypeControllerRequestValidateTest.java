6c44dafa913d00b969de34be581365581976a1ce
minsik2434 <minsik2434@gmail.com>
2025-08-05 19:09:44 +0900

    Test : OptionTypeController 검증 테스트
    OptionTypeRequest 검증 성공 테스트 작성

--- product-service/src/test/java/com/example/product_service/controller/validation/OptionTypeControllerRequestValidateTest.java
+++ product-service/src/test/java/com/example/product_service/controller/validation/OptionTypeControllerRequestValidateTest.java
@@ -2,12 +2,17 @@ package com.example.product_service.controller.validation;
 
 import com.example.product_service.controller.util.ValidationTestHelper;
 import com.example.product_service.dto.request.options.OptionTypeRequest;
+import jakarta.validation.ConstraintViolation;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Set;
+
 import static com.example.product_service.controller.util.ValidationTestHelper.*;
+import static org.assertj.core.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 public class OptionTypeControllerRequestValidateTest {
@@ -17,5 +22,13 @@ public class OptionTypeControllerRequestValidateTest {
     void optionTypeRequestValidation_field(){
         OptionTypeRequest request = new OptionTypeRequest("optionType");
         assertFieldViolation(request, "name", "", "name is required");
+    }
+
+    @Test
+    @DisplayName("OptionTypeRequest 검증 성공")
+    void optionTypeRequestValidation_thenOk(){
+        OptionTypeRequest request = new OptionTypeRequest("name");
+        Set<ConstraintViolation<OptionTypeRequest>> violations = validateField(request);
+        assertThat(violations).isEmpty();
     }
 }
