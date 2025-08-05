44e376000b2a318df6eccbdd03fa37c77ac90689
minsik2434 <minsik2434@gmail.com>
2025-08-05 21:37:57 +0900

    Refactor : TestMessageUtil
    ValidationTestHelper 테스트 메시지 부분 분리

---
+++ product-service/src/test/java/com/example/product_service/controller/util/TestMessageUtil.java
@@ -0,0 +1,20 @@
+package com.example.product_service.controller.util;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+import java.util.Locale;
+
+public final class TestMessageUtil {
+    private static final MessageSource messageSource;
+    static {
+        ReloadableResourceBundleMessageSource ms = new ReloadableResourceBundleMessageSource();
+        ms.setBasename("classpath:ValidationMessages"); // 메시지 파일 경로
+        ms.setDefaultEncoding("UTF-8");
+        messageSource = ms;
+    }
+
+    public static String getMessage(String code) {
+        return messageSource.getMessage(code, null, Locale.getDefault());
+    }
+}
