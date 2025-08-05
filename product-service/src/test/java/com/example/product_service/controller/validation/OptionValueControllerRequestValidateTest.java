bd1ba8f1f383ed3741ecfe663f91efb974772602
minsik2434 <minsik2434@gmail.com>
2025-08-05 21:35:46 +0900

    Feat : OptionValueControllerRequestValidateTest
    OptionValueRequest 전체 필드 검증 테스트 구현

--- product-service/src/test/java/com/example/product_service/controller/validation/OptionValueControllerRequestValidateTest.java
+++ product-service/src/test/java/com/example/product_service/controller/validation/OptionValueControllerRequestValidateTest.java
@@ -1,15 +1,21 @@
 package com.example.product_service.controller.validation;
 
 import com.example.product_service.dto.request.options.OptionValueRequest;
+import jakarta.validation.ConstraintViolation;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Stream;
 
 import static com.example.product_service.controller.util.ValidationTestHelper.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 public class OptionValueControllerRequestValidateTest {
@@ -21,6 +27,23 @@ public class OptionValueControllerRequestValidateTest {
     void optionValueRequestValidation_field(String fieldName, Object invalidValue, String expectedMessage){
         OptionValueRequest request = new OptionValueRequest(1L, "value");
         assertFieldViolation(request, fieldName, invalidValue, expectedMessage);
+    }
+
+    @Test
+    @DisplayName("OptionValueRequest 필드 동시 오류 발생시 전체 개수 및 필드 확인")
+    void optionValueRequestValidation_multiple(){
+        OptionValueRequest request = new OptionValueRequest(null, "");
+
+        Set<ConstraintViolation<OptionValueRequest>> violations = validateField(request);
+
+        assertThat(violations).hasSize(2);
+
+        List<String> fields = violations.stream()
+                .map(v -> v.getPropertyPath().toString())
+                .toList();
+
+        assertThat(fields)
+                .containsExactlyInAnyOrder("optionTypeId", "value");
     }
 
     static Stream<Arguments> invalidOptionValueRequestFieldProvider(){
