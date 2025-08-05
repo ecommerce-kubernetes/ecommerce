9bbf1e3f3841c383b25cb22b782dd712ad006f4f
minsik2434 <minsik2434@gmail.com>
2025-08-05 14:21:00 +0900

    Refactor : OptionValueControllerSecurityTest
    테스트 리팩터링

--- product-service/src/test/java/com/example/product_service/controller/auth/OptionValueControllerSecurityTest.java
+++ product-service/src/test/java/com/example/product_service/controller/auth/OptionValueControllerSecurityTest.java
@@ -4,6 +4,7 @@ import com.example.product_service.common.advice.CustomAccessDeniedHandler;
 import com.example.product_service.common.advice.CustomAuthenticationEntryPoint;
 import com.example.product_service.config.WebSecurity;
 import com.example.product_service.controller.OptionValueController;
+import com.example.product_service.controller.util.UserRole;
 import com.example.product_service.dto.request.options.OptionValueRequest;
 import com.example.product_service.dto.request.options.UpdateOptionValueRequest;
 import com.example.product_service.service.OptionValueService;
@@ -13,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -36,78 +36,56 @@ public class OptionValueControllerSecurityTest {
     @Test
     @DisplayName("옵션 값 저장 테스트-인증 에러")
     void createOptionValueTest_UnAuthorized() throws Exception {
-        String jsonBody = toJson(createOptionValueRequest());
-
-        ResultActions perform = mockMvc.perform(post(BASE_PATH)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(jsonBody));
-
+        ResultActions perform =
+                performWithAuthAndBody(mockMvc, post(BASE_PATH), createOptionValueRequest(), null);
         verifyUnauthorizedResponse(perform, BASE_PATH);
     }
 
     @Test
     @DisplayName("옵션 값 저장 테스트-권한 부족")
     void createOptionValueTest_NoPermission() throws Exception {
-        String jsonBody = toJson(createOptionValueRequest());
-
-        ResultActions perform = mockMvc.perform(post(BASE_PATH)
-                .header(USER_ID_HEADER, 1L)
-                .header(USER_ROLE_HEADER, USER_ROLE)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(jsonBody));
-
+        ResultActions perform =
+                performWithAuthAndBody(mockMvc, post(BASE_PATH), createOptionValueRequest(), UserRole.ROLE_USER);
         verifyNoPermissionResponse(perform, BASE_PATH);
     }
 
     @Test
     @DisplayName("옵션 값 수정 테스트-인증 에러")
     void updateOptionValueTest_UnAuthorized() throws Exception {
-        String jsonBody = toJson(createUpdateOptionValueRequest());
-
-        ResultActions perform = mockMvc.perform(patch(OPTION_VALUE_ID_PATH)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(jsonBody));
-
+        ResultActions perform =
+                performWithAuthAndBody(mockMvc, patch(OPTION_VALUE_ID_PATH), createUpdateOptionValueRequest(), null);
         verifyUnauthorizedResponse(perform, OPTION_VALUE_ID_PATH);
     }
 
     @Test
     @DisplayName("옵션 값 수정 테스트-권한 부족")
     void updateOptionValueTest_NoPermission() throws Exception {
-        String jsonBody = toJson(createUpdateOptionValueRequest());
-
-        ResultActions perform = mockMvc.perform(patch(OPTION_VALUE_ID_PATH)
-                        .header(USER_ID_HEADER, 1L)
-                        .header(USER_ROLE_HEADER, USER_ROLE)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(jsonBody));
-
+        ResultActions perform =
+                performWithAuthAndBody(mockMvc, patch(OPTION_VALUE_ID_PATH), createUpdateOptionValueRequest(), UserRole.ROLE_USER);
         verifyNoPermissionResponse(perform, OPTION_VALUE_ID_PATH);
     }
 
     @Test
     @DisplayName("옵션 값 삭제 테스트-인증 에러")
     void deleteOptionValueTest_UnAuthorized() throws Exception {
-        ResultActions perform = mockMvc.perform(delete(OPTION_VALUE_ID_PATH));
-
+        ResultActions perform =
+                performWithAuthAndBody(mockMvc, delete(OPTION_VALUE_ID_PATH), null, null);
         verifyUnauthorizedResponse(perform, OPTION_VALUE_ID_PATH);
     }
 
     @Test
     @DisplayName("옵션 값 삭제 테스트-권한 부족")
     void deleteOptionValueTest_NoPermission() throws Exception {
-        ResultActions perform = mockMvc.perform(delete(OPTION_VALUE_ID_PATH)
-                .header(USER_ID_HEADER, 1L)
-                .header(USER_ROLE_HEADER, USER_ROLE));
-
+        ResultActions perform =
+                performWithAuthAndBody(mockMvc, delete(OPTION_VALUE_ID_PATH), null, UserRole.ROLE_USER);
         verifyNoPermissionResponse(perform, OPTION_VALUE_ID_PATH);
     }
 
-    private static OptionValueRequest createOptionValueRequest() {
+    private OptionValueRequest createOptionValueRequest() {
         return new OptionValueRequest(1L, "value");
     }
 
-    private static UpdateOptionValueRequest createUpdateOptionValueRequest() {
+    private UpdateOptionValueRequest createUpdateOptionValueRequest() {
         return new UpdateOptionValueRequest(1L, "value");
     }
 }
