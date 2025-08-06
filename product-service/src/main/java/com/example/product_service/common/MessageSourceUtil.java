c0607336fed545677cb8af67ab8a57dc494822ef
minsik2434 <minsik2434@gmail.com>
2025-08-06 21:24:04 +0900

    Feat: 메시지 Util 생성, 메시지 추가

---
+++ product-service/src/main/java/com/example/product_service/common/MessageSourceUtil.java
@@ -0,0 +1,17 @@
+package com.example.product_service.common;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
+@Component
+@RequiredArgsConstructor
+public class MessageSourceUtil {
+    private final MessageSource messageSource;
+
+    public String getMessage(String code){
+        return messageSource.getMessage(code, null, Locale.getDefault());
+    }
+}
