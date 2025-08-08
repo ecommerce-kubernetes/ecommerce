cf44bc5fa7a53d3d4c15937d2b6179af1f04cf83
minsik2434 <minsik2434@gmail.com>
2025-08-09 03:28:56 +0900

    Refactor : ProductControllerRequestValidationTest 테스트 분리

--- product-service/src/test/java/com/example/product_service/controller/validation/ImageRequestValidationTest.java
+++ product-service/src/test/java/com/example/product_service/controller/validation/ImageRequestValidationTest.java
@@ -1,17 +1,23 @@
 package com.example.product_service.controller.validation;
 
 import com.example.product_service.dto.request.image.ImageRequest;
+import jakarta.validation.ConstraintViolation;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Stream;
 
 import static com.example.product_service.controller.util.MessagePath.*;
 import static com.example.product_service.controller.util.TestMessageUtil.getMessage;
 import static com.example.product_service.controller.util.ValidationTestHelper.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 public class ImageRequestValidationTest {
@@ -21,6 +27,25 @@ public class ImageRequestValidationTest {
     void ImageRequestValidation_field(String fieldName, Object invalidValue, String expectedMessage){
         ImageRequest request = new ImageRequest("http:test.jpg", 0);
         assertFieldViolation(request, fieldName, invalidValue, expectedMessage);
+    }
+
+    @Test
+    @DisplayName("ImageRequest 필드 동시 오류 발생시 전체 개수 및 필드 확인")
+    void imageRequestValidation_multiple(){
+        ImageRequest request = new ImageRequest("", -1);
+        Set<ConstraintViolation<ImageRequest>> violations = validateField(request);
+        List<String> fields = violations.stream().map(v -> v.getPropertyPath().toString()).toList();
+
+        assertThat(violations).hasSize(2);
+        assertThat(fields).containsExactlyInAnyOrder("url", "sortOrder");
+    }
+
+    @Test
+    @DisplayName("ImageRequest 오류 없음")
+    void imageRequestValidation_thenOk(){
+        ImageRequest request = new ImageRequest("http://test.jpg", 0);
+        Set<ConstraintViolation<ImageRequest>> violations = validateField(request);
+        assertThat(violations).isEmpty();
     }
 
     static Stream<Arguments> invalidImageRequestFieldProvider(){
---
+++ product-service/src/test/java/com/example/product_service/controller/validation/ProductRequestValidationTest.java
@@ -0,0 +1,80 @@
+package com.example.product_service.controller.validation;
+import com.example.product_service.dto.request.image.ImageRequest;
+import com.example.product_service.dto.request.options.ProductOptionTypeRequest;
+import com.example.product_service.dto.request.product.ProductRequest;
+import com.example.product_service.dto.request.variant.ProductVariantRequest;
+import jakarta.validation.ConstraintViolation;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static com.example.product_service.controller.util.MessagePath.*;
+import static com.example.product_service.controller.util.TestMessageUtil.*;
+import static com.example.product_service.controller.util.ValidationTestHelper.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+public class ProductRequestValidationTest {
+
+    @ParameterizedTest(name = "[{index}] {0} 필드 invalid")
+    @MethodSource("invalidProductRequestFieldProvider")
+    void productRequestValidation_field(String fieldName, Object invalidValue, String expectedMessage){
+        ProductRequest request = createProductRequest();
+        assertFieldViolation(request, fieldName, invalidValue, expectedMessage);
+    }
+
+    @Test
+    @DisplayName("ProductRequest 필드 동시 오류 발생시 전체 개수 및 필드 확인")
+    void productRequestValidation_multiple(){
+        ProductRequest productRequest = new ProductRequest();
+        Set<ConstraintViolation<ProductRequest>> violations = validateField(productRequest);
+        assertThat(violations).hasSize(4);
+        List<String> fields = violations.stream().map(v -> v.getPropertyPath().toString()).toList();
+        assertThat(fields).containsExactlyInAnyOrder("name", "categoryId", "images", "productVariants");
+    }
+
+    @Test
+    @DisplayName("ProductRequest 오류 없음")
+    void productRequestValidation_thenOk(){
+        ProductRequest request = createProductRequest();
+        Set<ConstraintViolation<ProductRequest>> violations = validateField(request);
+        assertThat(violations).isEmpty();
+    }
+
+    static Stream<Arguments> invalidProductRequestFieldProvider(){
+        return Stream.of(
+                Arguments.of("name", "", getMessage(NOT_BLANK)),
+                Arguments.of("categoryId", null, getMessage(NOT_NULL)),
+                Arguments.of("images", List.of(), getMessage(NOT_EMPTY)),
+                Arguments.of("images", null, getMessage(NOT_EMPTY)),
+                Arguments.of("productVariants", List.of(), getMessage(NOT_EMPTY))
+        );
+    }
+
+    private ProductRequest createProductRequest() {
+        return new ProductRequest("name",
+                "description",
+                1L,
+                List.of(new ImageRequest("http://test.jpg", 0)),
+                List.of(new ProductOptionTypeRequest(1L, 0)),
+                createProductVariantRequestList());
+    }
+    private List<ProductVariantRequest> createProductVariantRequestList(){
+        return List.of(createProductVariantRequest());
+    }
+
+    private ProductVariantRequest createProductVariantRequest(){
+        return new ProductVariantRequest(
+                "sku", 100, 100, 10,
+                List.of(1L, 2L)
+        );
+    }
+}
