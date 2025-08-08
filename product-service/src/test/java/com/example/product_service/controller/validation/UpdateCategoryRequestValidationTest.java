78fe100504be70cc67e8ae3f1af84fdfa144e9d6
minsik2434 <minsik2434@gmail.com>
2025-08-09 03:16:53 +0900

    Refactor : UpdateCategoryRequestValidationTest 테스트 분리

--- product-service/src/test/java/com/example/product_service/controller/validation/UpdateCategoryRequestValidationTest.java
+++ product-service/src/test/java/com/example/product_service/controller/validation/UpdateCategoryRequestValidationTest.java
@@ -1,6 +1,5 @@
 package com.example.product_service.controller.validation;
 
-import com.example.product_service.dto.request.category.CategoryRequest;
 import com.example.product_service.dto.request.category.UpdateCategoryRequest;
 import jakarta.validation.ConstraintViolation;
 import org.junit.jupiter.api.DisplayName;
@@ -16,53 +15,22 @@ import java.util.Set;
 import java.util.stream.Stream;
 
 import static com.example.product_service.controller.util.MessagePath.*;
-import static com.example.product_service.controller.util.TestMessageUtil.*;
-import static com.example.product_service.controller.util.ValidationTestHelper.*;
-import static org.assertj.core.api.Assertions.*;
+import static com.example.product_service.controller.util.TestMessageUtil.getMessage;
+import static com.example.product_service.controller.util.ValidationTestHelper.assertFieldViolation;
+import static com.example.product_service.controller.util.ValidationTestHelper.validateField;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
-public class CategoryControllerRequestValidateTest {
+public class UpdateCategoryRequestValidationTest {
+
     private static final String INVALID_URL = "invalidUrl";
     private static final String VALID_URL = "http://test.jpg";
-
-    @ParameterizedTest(name = "[{index}] {0} 필드 invalid")
-    @MethodSource("invalidCategoryRequestFieldProvider")
-    void categoryRequestValidation_field(String fieldName, Object invalidValue, String expectedMessage){
-        CategoryRequest request =
-                new CategoryRequest("노트북", 1L, VALID_URL);
-        assertFieldViolation(request, fieldName, invalidValue, expectedMessage);
-    }
 
     @ParameterizedTest(name = "[{index}] {0} 필드 invalid")
     @MethodSource("invalidUpdateCategoryRequestFieldProvider")
     void updateCategoryRequestValidation_field(String fieldName, Object invalidValue, String expectedMessage){
         UpdateCategoryRequest request = new UpdateCategoryRequest("노트북", 1L, VALID_URL);
         assertFieldViolation(request, fieldName, invalidValue, expectedMessage);
-    }
-
-    @Test
-    @DisplayName("Category Request 필드 동시 오류 발생시 전체 개수 및 필드 확인")
-    void categoryRequestValidation_multiple(){
-        CategoryRequest request = new CategoryRequest("", 1L, INVALID_URL);
-
-        Set<ConstraintViolation<CategoryRequest>> violations = validateField(request);
-
-        assertThat(violations).hasSize(2);
-
-        List<String> fields = violations.stream()
-                .map(v -> v.getPropertyPath().toString())
-                .toList();
-
-        assertThat(fields)
-                .containsExactlyInAnyOrder("name", "iconUrl");
-    }
-
-    @Test
-    @DisplayName("Category Request 오류 없음")
-    void categoryRequestValidation_thenOk(){
-        CategoryRequest categoryRequest = new CategoryRequest("name", 1L, VALID_URL);
-        Set<ConstraintViolation<CategoryRequest>> violations = validateField(categoryRequest);
-        assertThat(violations).isEmpty();
     }
 
     @Test
@@ -83,14 +51,6 @@ public class CategoryControllerRequestValidateTest {
                 .map(ConstraintViolation::getMessage).toList();
 
         assertThat(messages).containsExactlyInAnyOrder(getMessage(EMPTY_REQUEST));
-    }
-
-    static Stream<Arguments> invalidCategoryRequestFieldProvider(){
-        return Stream.of(
-                Arguments.of("name", "", getMessage(NOT_BLANK)),
-                Arguments.of("name", " ", getMessage(NOT_BLANK)),
-                Arguments.of("iconUrl", INVALID_URL, getMessage(INVALID_URL_MESSAGE))
-        );
     }
 
     static Stream<Arguments> invalidUpdateCategoryRequestFieldProvider(){
