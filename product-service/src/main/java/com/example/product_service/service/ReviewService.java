7a0a575a8f6eb8746ecd632cca8de5376125673c
minsik2434 <minsik2434@gmail.com>
2025-08-26 21:08:46 +0900

    Feat: 리뷰 인터페이스 삭제

--- product-service/src/main/java/com/example/product_service/service/ReviewService.java
+++ product-service/src/main/java/com/example/product_service/service/ReviewService.java
@@ -1,5 +1,22 @@
 package com.example.product_service.service;
 
-public interface ReviewService {
-    void deleteReviewById(Long reviewId, Long userId);
+import com.example.product_service.repository.ReviewsRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Slf4j
+public class ReviewService {
+
+    private final ReviewsRepository reviewsRepository;
+
+
+    public void deleteReviewById(Long reviewId, Long userId) {
+
+    }
 }
