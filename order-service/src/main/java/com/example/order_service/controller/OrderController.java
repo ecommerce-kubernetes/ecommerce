99a7932ef711f509048381612bf05d6003385b03
minsik2434 <minsik2434@gmail.com>
2025-08-31 17:12:45 +0900

    Feat: 주문 컨트롤러 API 명세

--- order-service/src/main/java/com/example/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/example/order_service/controller/OrderController.java
@@ -1,9 +1,13 @@
 package com.example.order_service.controller;
 
+import com.example.order_service.controller.util.specification.annotation.BadRequestApiResponse;
+import com.example.order_service.controller.util.specification.annotation.NotFoundApiResponse;
 import com.example.order_service.dto.request.OrderRequest;
 import com.example.order_service.dto.response.OrderResponseDto;
 import com.example.order_service.dto.response.PageDto;
 import com.example.order_service.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpStatus;
@@ -16,12 +20,14 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Order", description = "주문 관련 API")
 @RequestMapping("/orders")
-@Slf4j
 public class OrderController {
 
     private final OrderService orderService;
 
+    @Operation(summary = "주문 생성")
+    @NotFoundApiResponse @BadRequestApiResponse
     @PostMapping
     public ResponseEntity<OrderResponseDto> createOrder(@RequestBody @Validated OrderRequest orderRequest,
                                                         @RequestHeader("user-id") String userIdHeader){
@@ -31,6 +37,8 @@ public class OrderController {
         return ResponseEntity.status(HttpStatus.SC_CREATED).body(orderResponseDto);
     }
 
+    @Operation(summary = "주문 목록 조회")
+    @BadRequestApiResponse
     @GetMapping("/{userId}")
     public ResponseEntity<PageDto<OrderResponseDto>> getAllOrdersByUserId(@PathVariable("userId") Long userId,
                                                                   @RequestParam(value = "page", defaultValue = "0") int page,
