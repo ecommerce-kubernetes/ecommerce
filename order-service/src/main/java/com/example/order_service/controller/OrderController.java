b1ff55560df5d704aa08d3c8a8840b5aee91963d
minsik2434 <minsik2434@gmail.com>
2025-09-01 15:25:22 +0900

    Feat: 주문 목록 조회 테스트

--- order-service/src/main/java/com/example/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/example/order_service/controller/OrderController.java
@@ -3,9 +3,12 @@ package com.example.order_service.controller;
 import com.example.order_service.controller.util.specification.annotation.BadRequestApiResponse;
 import com.example.order_service.controller.util.specification.annotation.ConflictApiResponse;
 import com.example.order_service.controller.util.specification.annotation.NotFoundApiResponse;
+import com.example.order_service.controller.util.validator.PageableValidator;
+import com.example.order_service.controller.util.validator.PageableValidatorFactory;
 import com.example.order_service.dto.request.OrderRequest;
 import com.example.order_service.dto.response.OrderResponse;
 import com.example.order_service.dto.response.PageDto;
+import com.example.order_service.entity.DomainType;
 import com.example.order_service.service.OrderService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -25,6 +28,7 @@ import org.springframework.web.bind.annotation.*;
 public class OrderController {
 
     private final OrderService orderService;
+    private final PageableValidatorFactory factory;
 
     @Operation(summary = "주문 생성")
     @NotFoundApiResponse @BadRequestApiResponse @ConflictApiResponse
@@ -38,11 +42,13 @@ public class OrderController {
     @Operation(summary = "주문 목록 조회")
     @BadRequestApiResponse
     @GetMapping
-    public ResponseEntity<PageDto<OrderResponse>> getAllOrdersByUserId(@RequestHeader("X-User-Id") Long userId,
+    public ResponseEntity<PageDto<OrderResponse>> getOrders(@RequestHeader("X-User-Id") Long userId,
                                                                        @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable,
                                                                        @RequestParam(value = "year",required = false) String year,
                                                                        @RequestParam(value = "keyword", required = false) String keyword){
-        PageDto<OrderResponse> orderList = orderService.getOrderList(pageable, userId, year, keyword);
+        PageableValidator validator = factory.getValidator(DomainType.ORDER);
+        Pageable validatedPageable = validator.validate(pageable);
+        PageDto<OrderResponse> orderList = orderService.getOrderList(validatedPageable, userId, year, keyword);
         return ResponseEntity.ok(orderList);
     }
 }
--- order-service/src/test/java/com/example/order_service/controller/OrderControllerTest.java
+++ order-service/src/test/java/com/example/order_service/controller/OrderControllerTest.java
@@ -2,9 +2,12 @@ package com.example.order_service.controller;
 
 import com.example.order_service.common.MessageSourceUtil;
 import com.example.order_service.common.advice.ErrorResponseEntityFactory;
+import com.example.order_service.controller.util.validator.OrderPageableValidator;
+import com.example.order_service.controller.util.validator.PageableValidatorFactory;
 import com.example.order_service.dto.request.OrderItemRequest;
 import com.example.order_service.dto.request.OrderRequest;
 import com.example.order_service.dto.response.*;
+import com.example.order_service.entity.DomainType;
 import com.example.order_service.exception.BadRequestException;
 import com.example.order_service.exception.InsufficientException;
 import com.example.order_service.exception.InvalidResourceException;
@@ -16,6 +19,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -25,19 +29,16 @@ import java.util.List;
 
 import static com.example.order_service.common.MessagePath.*;
 import static com.example.order_service.common.MessagePath.BAD_REQUEST_VALIDATION;
-import static com.example.order_service.util.ControllerTestHelper.performWithBodyAndUserIdHeader;
-import static com.example.order_service.util.ControllerTestHelper.verifyErrorResponse;
+import static com.example.order_service.util.ControllerTestHelper.*;
 import static com.example.order_service.util.TestMessageUtil.getMessage;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.tuple;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(OrderController.class)
-@Import(ErrorResponseEntityFactory.class)
+@Import({ErrorResponseEntityFactory.class})
 class OrderControllerTest {
 
     private static final String BASE_PATH = "/orders";
@@ -48,6 +49,8 @@ class OrderControllerTest {
     OrderService orderService;
     @MockitoBean
     MessageSourceUtil ms;
+    @MockitoBean
+    PageableValidatorFactory pageableValidatorFactory;
 
     @BeforeEach
     void setUpMessages() {
@@ -59,7 +62,7 @@ class OrderControllerTest {
 
     @Test
     @DisplayName("주문 생성 테스트-성공")
-    void createOrderTest_success(){
+    void createOrderTest_success() throws Exception {
         OrderResponse expectedResponse = new OrderResponse(
                 1L, "PENDING", LocalDateTime.now(), "서울시 테헤란로 123",
                 new OrderSummary(6000, 1100, 4900),
@@ -76,39 +79,20 @@ class OrderControllerTest {
         OrderRequest request = new OrderRequest(
                 List.of(new OrderItemRequest(1L, 2)),
                 "서울시 테헤란로 123", 1L, 4000, 900);
-        OrderResponse response = orderService.saveOrder(1L, request);
+        ResultActions perform = performWithBodyAndUserIdHeader(mockMvc, post(BASE_PATH), request);
+        verifySuccessResponse(perform, status().isCreated(), expectedResponse);
+    }
 
+    @Test
+    @DisplayName("주문 생성 테스트-실패(헤더 없음)")
+    void createOrderTest_noHeader() throws Exception {
+        List<OrderItemRequest> orderItemRequests = List.of(new OrderItemRequest(1L, 10));
+        OrderRequest request = new OrderRequest(orderItemRequests, "서울시 테헤란로 234", 1L, 3000, 5000);
 
-        assertThat(response)
-                .extracting(OrderResponse::getId, OrderResponse::getStatus, OrderResponse::getDeliveryAddress)
-                .containsExactlyInAnyOrder(1L, "PENDING", "서울시 테헤란로 123");
-
-        assertThat(response.getOrderSummary())
-                .extracting(OrderSummary::getProductTotal, OrderSummary::getDiscount, OrderSummary::getFinalPayment)
-                .containsExactlyInAnyOrder(6000, 1100, 4900);
-
-        assertThat(response.getPaymentDetails())
-                .extracting(PaymentDetails::getUsedCash, PaymentDetails::getUsedReserve)
-                .containsExactlyInAnyOrder(4000, 900);
-
-        assertThat(response.getCouponDetails())
-                .extracting(CouponDetails::getCouponId, CouponDetails::getDiscountAmount)
-                .containsExactlyInAnyOrder(1L, 500);
-
-        assertThat(response.getOrderItemSummary().getItemFinalPrice()).isEqualTo(5400);
-        assertThat(response.getOrderItemSummary().getItems())
-                .extracting(OrderItemResponse::getProductId, OrderItemResponse::getProductName, OrderItemResponse::getPrice,
-                        OrderItemResponse::getQuantity, OrderItemResponse::getDiscountRate, OrderItemResponse::getThumbNailUrl)
-                .containsExactlyInAnyOrder(
-                        tuple(1L, "상품1", 3000, 2, 10, "http://product1.jpg")
-                );
-
-        assertThat(response.getOrderItemSummary().getItems())
-                .flatExtracting(OrderItemResponse::getOptions)
-                .extracting(ItemOptionResponse::getOptionTypeName, ItemOptionResponse::getOptionValueName)
-                .containsExactlyInAnyOrder(
-                    tuple("색상", "RED")
-                );
+        ResultActions perform = performWithBody(mockMvc, post(BASE_PATH), request);
+        verifyErrorResponse(perform, status().isBadRequest(), getMessage(BAD_REQUEST),
+                "Required request header 'X-User-Id' for method parameter type Long is not present",
+                BASE_PATH);
     }
 
     @Test
@@ -186,5 +170,38 @@ class OrderControllerTest {
 
         ResultActions perform = performWithBodyAndUserIdHeader(mockMvc, post(BASE_PATH), request);
         verifyErrorResponse(perform, status().isBadRequest(), getMessage(BAD_REQUEST), getMessage(NOT_MATCH_ORDER_AMOUNT), BASE_PATH);
+    }
+
+    @Test
+    @DisplayName("주문 목록 조회 테스트-성공")
+    void getOrdersTest_success() throws Exception {
+        OrderResponse expectedResponse = new OrderResponse(
+                1L, "COMPLETED", LocalDateTime.now(), "서울시 테헤란로 123",
+                new OrderSummary(6000, 1100, 4900),
+                new PaymentDetails(4000, 900),
+                new CouponDetails(1L, 500),
+                new OrderItemSummary(
+                        List.of(new OrderItemResponse(1L, "상품1", 2, 3000, 10,
+                                List.of(new ItemOptionResponse("색상", "RED")),
+                                "http://product1.jpg")), 5400)
+        );
+        PageDto<OrderResponse> orderResponsePageDto = new PageDto<>(List.of(expectedResponse), 1, 10, 10, 20);
+        when(orderService.getOrderList(any(Pageable.class), anyLong(), any(), any()))
+                .thenReturn(orderResponsePageDto);
+        when(pageableValidatorFactory.getValidator(DomainType.ORDER)).thenReturn(new OrderPageableValidator());
+        ResultActions perform = performWithBodyAndUserIdHeader(mockMvc,
+                get(BASE_PATH).param("page", "1").param("size", "10"),
+                null);
+
+        verifySuccessResponse(perform, status().isOk(), orderResponsePageDto);
+    }
+
+    @Test
+    @DisplayName("주문 목록 조회 테스트-실패(헤더 없음)")
+    void getOrdersTest_noHeader() throws Exception {
+        ResultActions perform = performWithBody(mockMvc, get(BASE_PATH), null);
+        verifyErrorResponse(perform, status().isBadRequest(), getMessage(BAD_REQUEST),
+                "Required request header 'X-User-Id' for method parameter type Long is not present",
+                BASE_PATH);
     }
 }
