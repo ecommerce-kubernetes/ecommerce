2c26f86a3b4448c2388bbcb8935aafa8e620642e
minsik2434 <minsik2434@gmail.com>
2025-12-12 16:29:57 +0900

    주문 금액 계산 객체 생성

--- order-service/src/main/java/com/example/order_service/api/order/domain/service/OrderPriceCalculator.java
+++ order-service/src/main/java/com/example/order_service/api/order/domain/service/OrderPriceCalculator.java
@@ -1,9 +1,12 @@
 package com.example.order_service.api.order.domain.service;
 
+import com.example.order_service.api.common.exception.InsufficientException;
+import com.example.order_service.api.common.exception.OrderVerificationException;
 import com.example.order_service.api.order.application.dto.command.CreateOrderItemDto;
 import com.example.order_service.api.order.application.dto.context.PriceCalculateResult;
 import com.example.order_service.api.order.infrastructure.client.coupon.dto.OrderCouponCalcResponse;
 import com.example.order_service.api.order.infrastructure.client.product.dto.OrderProductResponse;
+import com.example.order_service.api.order.infrastructure.client.user.dto.OrderUserResponse;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -13,19 +16,9 @@ import java.util.stream.Collectors;
 @Component
 public class OrderPriceCalculator {
 
-    public long calculateRowTotalPrice(List<CreateOrderItemDto> items, List<OrderProductResponse> products) {
-        Map<Long, Integer> quantityByVariantId = items.stream()
-                .collect(Collectors.toMap(
-                        CreateOrderItemDto::getProductVariantId,
-                        CreateOrderItemDto::getQuantity
-                ));
-
-        Map<Long, OrderProductResponse.UnitPrice> unitPriceByVariantId = products.stream()
-                .collect(Collectors.toMap(
-                        OrderProductResponse::getProductVariantId,
-                        OrderProductResponse::getUnitPrice
-                ));
-
+    public long calculateSubTotalPrice(List<CreateOrderItemDto> items, List<OrderProductResponse> products) {
+        Map<Long, Integer> quantityByVariantId = mapToQuantityByVariantId(items);
+        Map<Long, OrderProductResponse.UnitPrice> unitPriceByVariantId = mapToUnitPriceByVariantId(products);
         return quantityByVariantId.entrySet().stream()
                 .mapToLong(
                         entry -> entry.getValue() * unitPriceByVariantId
@@ -33,7 +26,41 @@ public class OrderPriceCalculator {
                 .sum();
     }
 
-    public PriceCalculateResult calculateFinalPrice(OrderCouponCalcResponse coupon){
-        return null;
+    public PriceCalculateResult calculateFinalPrice(long useToPoint, long subTotal, long expectedPrice,
+                                                    OrderUserResponse user, OrderCouponCalcResponse coupon) {
+        verifyEnoughPoints(useToPoint, user);
+        long discountAmount = coupon != null ? coupon.getDiscountAmount() : 0L;
+        long priceAfterCoupon = subTotal - discountAmount;
+        long finalPaymentPrice = priceAfterCoupon - useToPoint;
+
+        if(finalPaymentPrice != expectedPrice) {
+            throw new OrderVerificationException("주문 금액이 변동되었습니다");
+        }
+        return PriceCalculateResult.of(subTotal, finalPaymentPrice, coupon, useToPoint);
     }
+
+    private Map<Long, OrderProductResponse.UnitPrice> mapToUnitPriceByVariantId(List<OrderProductResponse> products) {
+        return products.stream()
+                .collect(Collectors.toMap(
+                        OrderProductResponse::getProductVariantId,
+                        OrderProductResponse::getUnitPrice
+                ));
+    }
+
+    private void verifyEnoughPoints(Long useToPoint, OrderUserResponse user){
+        if(useToPoint != null && useToPoint > 0) {
+            if(useToPoint > user.getPointBalance()){
+                throw new InsufficientException("포인트가 부족합니다");
+            }
+        }
+    }
+
+    private Map<Long, Integer> mapToQuantityByVariantId(List<CreateOrderItemDto> items){
+        return  items.stream()
+                .collect(Collectors.toMap(
+                        CreateOrderItemDto::getProductVariantId,
+                        CreateOrderItemDto::getQuantity
+                ));
+    }
+
 }
---
+++ order-service/src/test/java/com/example/order_service/api/order/domain/OrderPriceCalculatorTest.java
@@ -0,0 +1,143 @@
+package com.example.order_service.api.order.domain;
+
+import com.example.order_service.api.common.exception.InsufficientException;
+import com.example.order_service.api.common.exception.OrderVerificationException;
+import com.example.order_service.api.order.application.dto.command.CreateOrderItemDto;
+import com.example.order_service.api.order.application.dto.context.PriceCalculateResult;
+import com.example.order_service.api.order.domain.service.OrderPriceCalculator;
+import com.example.order_service.api.order.infrastructure.client.coupon.dto.OrderCouponCalcResponse;
+import com.example.order_service.api.order.infrastructure.client.product.dto.OrderProductResponse;
+import com.example.order_service.api.order.infrastructure.client.user.dto.OrderUserResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@ExtendWith(MockitoExtension.class)
+public class OrderPriceCalculatorTest {
+
+    private final OrderPriceCalculator calculator = new OrderPriceCalculator();
+
+    @Test
+    @DisplayName("주문 상품의 총 가격을 계산한다 (할인된 상품 가격 * 주문 수량)")
+    void calculateSubTotalPrice() {
+        //given
+        CreateOrderItemDto item1 = CreateOrderItemDto.builder()
+                .productVariantId(1L)
+                .quantity(3)
+                .build();
+
+        CreateOrderItemDto item2 = CreateOrderItemDto.builder()
+                .productVariantId(2L)
+                .quantity(5)
+                .build();
+        OrderProductResponse product1 = createProductResponse(1L, 1L, "상품1", 3000L, 10,
+                "http://thumbnail.jpg", List.of());
+        OrderProductResponse product2 = createProductResponse(2L, 2L, "상품2", 5000L, 10,
+                "http://thumbnail.jpg", List.of());
+
+        List<CreateOrderItemDto> requestItems = List.of(item1, item2);
+        List<OrderProductResponse> responseItems = List.of(product1, product2);
+        //when
+        long subTotalPrice = calculator.calculateSubTotalPrice(requestItems, responseItems);
+        //then
+        assertThat(subTotalPrice).isEqualTo(30600L);
+    }
+
+    @Test
+    @DisplayName("쿠폰 할인가격이 적용된 최종 가격 정보를 계산한다")
+    void calculateFinalPrice() {
+        //given
+        OrderUserResponse user = OrderUserResponse.builder()
+                .userId(1L)
+                .pointBalance(4000L)
+                .build();
+
+        OrderCouponCalcResponse coupon = OrderCouponCalcResponse.builder()
+                .couponId(1L)
+                .couponName("1000원 할인 쿠폰")
+                .discountAmount(1000L)
+                .build();
+        //when
+        PriceCalculateResult result = calculator.calculateFinalPrice(1000, 30600L, 28600L, user, coupon);
+        //then
+        assertThat(result)
+                .extracting("originTotalPrice", "finalPaymentAmount", "useToPoint")
+                .contains(30600L, 28600L, 1000L);
+        assertThat(result.getCoupon())
+                .extracting("couponId", "couponName", "discountAmount")
+                .contains(1L, "1000원 할인 쿠폰", 1000L);
+    }
+    
+    @Test
+    @DisplayName("쿠폰을 사용하지 않은 최종 가격 정보를 계산한다")
+    void calculateFinalPrice_When_Coupon_Not_Used() {
+        //given
+        OrderUserResponse user = OrderUserResponse.builder()
+                .userId(1L)
+                .pointBalance(4000L)
+                .build();
+        //when
+        PriceCalculateResult result = calculator.calculateFinalPrice(1000, 30600L, 29600L, user, null);
+        //then
+        assertThat(result)
+                .extracting("originTotalPrice", "finalPaymentAmount", "useToPoint")
+                .contains(30600L, 29600L, 1000L);
+        assertThat(result.getCoupon()).isNull();
+    }
+    
+    @Test
+    @DisplayName("최종 결제금액을 계산할때 포인트가 부족한 경우 예외를 던진다")
+    void calculateFinalPrice_When_Not_Enough_Point() {
+        //given
+        OrderUserResponse user = OrderUserResponse.builder()
+                .userId(1L)
+                .pointBalance(100L)
+                .build();
+        //when
+        //then
+        assertThatThrownBy(() -> calculator.calculateFinalPrice(1000, 30600L, 29600L, user, null))
+                .isInstanceOf(InsufficientException.class)
+                .hasMessage("포인트가 부족합니다");
+    }
+
+    @Test
+    @DisplayName("최종 결제 금액이 예상 결제 금액과 동일하지 않은 경우 예외를 던진다")
+    void calculateFinalPrice_When_Not_Match_ExpectedPrice() {
+        //given
+        OrderUserResponse user = OrderUserResponse.builder()
+                .userId(1L)
+                .pointBalance(1000L)
+                .build();
+        //when
+        //then
+        assertThatThrownBy(() -> calculator.calculateFinalPrice(1000, 30600L, 29500L, user, null))
+                .isInstanceOf(OrderVerificationException.class)
+                .hasMessage("주문 금액이 변동되었습니다");
+    }
+
+    private OrderProductResponse createProductResponse(Long productId, Long productVariantId,
+                                                       String productName, Long originalPrice, int discountRate,
+                                                       String thumbnail, List<OrderProductResponse.ItemOption> options){
+        long discountAmount = originalPrice * discountRate / 100;
+        return OrderProductResponse.builder()
+                .productId(productId)
+                .productVariantId(productVariantId)
+                .productName(productName)
+                .unitPrice(
+                        OrderProductResponse.UnitPrice.builder()
+                                .originalPrice(originalPrice)
+                                .discountRate(discountRate)
+                                .discountAmount(discountAmount)
+                                .discountedPrice(originalPrice - discountAmount)
+                                .build())
+                .thumbnailUrl(thumbnail)
+                .itemOptions(options)
+                .build();
+    }
+}
