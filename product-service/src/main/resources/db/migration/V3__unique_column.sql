ee1cc791a1eaac897502b67a166b18ca82c297cc
minsik2434 <minsik2434@gmail.com>
2026-02-02 12:21:48 +0900

    Feat: 설정파일 수정

--- product-service/src/main/resources/db/migration/V3__unique_column.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE category ADD CONSTRAINT unique_categoryName UNIQUE (name);
-ALTER TABLE option_type ADD CONSTRAINT unique_option_type_name UNIQUE (name);
-ALTER TABLE product_variant ADD CONSTRAINT unique_product_variant_sku UNIQUE (sku);
