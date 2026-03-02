deb79d4bdd4cba30cc98db3c97ccf30606d86a44
minsik2434 <minsik2434@gmail.com>
2026-03-02 21:29:17 +0900

    Fix: import 경로 수정 import 문이 deprecated 되어 수정 Resolves: #355

--- user-service/k6/scripts/auth/login-test.js
+++ user-service/k6/scripts/auth/login-test.js
@@ -1,7 +1,7 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 import { SharedArray } from 'k6/data';
-import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bencuk.js";
+import { htmlReport } from "https://cdn.jsdelivr.net/gh/benc-uk/k6-reporter/dist/bundle.js";
 import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
 
 const users = new SharedArray('users', function () {
