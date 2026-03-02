56101a022dc1aef11f3cc21d94d9995c06dca09c
minsik2434 <minsik2434@gmail.com>
2026-03-02 21:18:38 +0900

    Fix: k6 스크립트 실행 경로 수정 유저 로그인 k6 run.sh 실행 경로 파라미터 변경 Resolves: #355

--- user-service/k6/run.sh
+++ user-service/k6/run.sh
@@ -5,17 +5,18 @@ set -a
 set +a
 
 MODE=$1
-SCRIPT=${2:-login-test.js}
+SCRIPT=${2:-auth/login-test.js}
+PATH="scripts/$SCRIPT"
 
-if [ ! -f "scripts/$SCRIPT" ]; then
-    echo "파일이 존재하지 않습니다: scripts/$SCRIPT"
+if [ ! -f "$PATH" ]; then
+    echo "파일이 존재하지 않습니다: $PATH"
     exit 1
 fi
 
 if [ "$MODE" == "prom"]; then
-    echo "Prometheus 지표 연동 실행 : scripts/$SCRIPT"
-    k6 run -o experimental-prometheus-rw "scripts/$SCRIPT"
+    echo "Prometheus 지표 연동 실행 : $PATH"
+    k6 run -o experimental-prometheus-rw "$PATH"
 else 
-    echo "일반 테스트 진행 (HTML 결과만 생성): scripts/$SCRIPT"
-    k6 run "scripts/$SCRIPT"
+    echo "일반 테스트 진행 (HTML 결과만 생성): $PATH"
+    k6 run "$PATH"
 fi
