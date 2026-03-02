bc6cb3b9a00737ee47afa02fef53663918b8efb4
minsik2434 <minsik2434@gmail.com>
2026-03-02 21:25:41 +0900

    Fix: run.sh 오타 수정
    Resolves: #355

--- user-service/k6/run.sh
+++ user-service/k6/run.sh
@@ -6,17 +6,17 @@ set +a
 
 MODE=$1
 SCRIPT=${2:-auth/login-test.js}
-PATH="scripts/$SCRIPT"
+FULL_PATH="scripts/$SCRIPT"
 
-if [ ! -f "$PATH" ]; then
-    echo "파일이 존재하지 않습니다: $PATH"
+if [ ! -f "$FULL_PATH" ]; then
+    echo "파일이 존재하지 않습니다: $FULL_PATH"
     exit 1
 fi
 
 if [ "$MODE" = "prom" ]; then
-    echo "Prometheus 지표 연동 실행 : $PATH"
-    k6 run -o experimental-prometheus-rw "$PATH"
+    echo "Prometheus 지표 연동 실행 : $FULL_PATH"
+    k6 run -o experimental-prometheus-rw "$FULL_PATH"
 else 
-    echo "일반 테스트 진행 (HTML 결과만 생성): $PATH"
-    k6 run "$PATH"
+    echo "일반 테스트 진행 (HTML 결과만 생성): $FULL_PATHH"
+    k6 run "$FULL_PATH"
 fi
