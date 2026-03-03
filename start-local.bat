46fab558d695a92986f69817c0d62fa9fbbcc062
minsik2434 <minsik2434@gmail.com>
2026-03-03 22:45:21 +0900

    Chore: 윈도우 실행 bat 작성 개발 환경 시스템 실행 윈도우 bat 작성 Resolves: #359

--- start-local.bat
+++ start-local.bat
@@ -1,17 +1,41 @@
 @echo off
 chcp 65001 > nul
+setlocal enabledelayedexpansion
 
-echo [0/3] 전체 마이크로 서비스 빌드
-call gradlew.bat clean build -x test
+echo [0/4] 마이크로 서비스 빌드
+set SERVICES=config-service discovery-service, order-service product-service user-service gateway-service
 
-echo [1/3] 도커 네트워크 생성
+for %%s in (%SERVICES%) do (
+    echo.
+    echo 빌드 중: %%s
+    cd %%s
+
+    findStr "asciidoctor" build.gradle >nul
+    if !errorlevel! equ 0 (
+        echo - asciidoctor 작업 제외하고 빌드 실행
+        call gradlew.bat clean build -x test -x asciidoctor
+    ) else (
+        echo - 빌드 실행
+        call gradlew.bat clean build -x test
+    )
+    
+    cd ..
+)
+
+echo.
+echo [1/4] 도커 네트워크 생성
 docker network inspect buynest-network >nul 2>&1 || docker network create buynest-network
 
-echo [2/3] 미들웨어 인프라 기동
-docker compose -f infra/middleware/docker-compose.yml up -d
+echo [2/4] 데이터베이스 컨테이너 실행
+docker compose -f ./infra/database/docker-compose.yml up -d
 
-echo 미들웨어 초기화 대기 (10초)...
+echo [3/4] 미들웨어 컨테이너 실행 
+docker compose -f ./infra/middleware/docker-compose.yml up -d 
+
+echo 미들웨어 초기화 대기
 timeout /t 10 /nobreak > nul
 
-echo [3/3] 마이크로 서비스 기동
+echo [4/4] 마이크로 서비스 도커 빌드 실행
 docker compose -f docker-compose-local.yml up -d --build
+
+echo 모든 작업 완료
