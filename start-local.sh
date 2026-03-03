a98bdd619dfbc85389f6863c6c350bf9ab3a8901
minsik2434 <minsik2434@gmail.com>
2026-03-03 18:13:29 +0900

    Feat: 로컬 실행 sh 작성 로컬 환경에서 시스템 실행 sh 구현 Resolves: #359

--- start-local.sh
+++ start-local.sh
@@ -1,13 +1,37 @@
 #!/bin/bash
-echo "[1/3] 도커 네트워크 생성"
+echo "[0/4] 마이크로 서비스 빌드"
+SERVICES=(config-service discovery-service gateway-service user-service order-service product-service)
+
+for SERVICE in "${SERVICES[@]}"; do
+    echo "빌드 중: $SERVICE"
+    cd "$SERVICE" || exit
+    chmod +x gradlew
+
+    if grep -q "asciidoctor" build.gradle; then
+        echo " asciidoctor 작업 제외하고 빌드 실행"
+        ./gradlew clean build -x test -x asciidoctor
+    else
+        echo "빌드 실행"
+        ./gradlew clean build -x test
+    fi
+
+    cd ..
+done
+
+echo "[1/4] 도커 네트워크 생성"
 docker network inspect buynest-network >/dev/null 2>&1 || \
     docker network create buynest-network
 
-echo "[2/3] 미들웨어 인프라 기동"
-docker compose -f infra/middleware/docker-compose.yml up -d 
+echo "[2/4]데이터베이스 컨테이너 실행"
+docker-compose -f ./infra/database/docker-compose.yml up -d
+
+echo "[3/4] 미들웨어 컨테이너 실행"
+docker-compose -f ./infra/middleware/docker-compose.yml up -d
 
 echo "미들웨어 초기화"
 sleep 10
 
-echo "[3/3] 마이크로 서비스 기동"
-docker-compose -f docker-compose-local.yml up -d --build
+echo "[4/4] 마이크로 서비스 도커 빌드 실행"
+docker compose -f docker-compose-local.yml up -d --build
+
+echo "모든 작업 완료"
