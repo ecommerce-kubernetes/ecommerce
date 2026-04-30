e87fafe521d1bd465bac4ae464532a1498139f23
minsik2434 <minsik2434@gmail.com>
2026-05-01 00:38:09 +0900

    Feat: 새 decoder 로직 통과를 위한 predicate 설정 수정

--- order-service/src/main/java/com/example/order_service/api/common/config/CircuitBreakerFailurePredicate.java
+++ order-service/src/main/java/com/example/order_service/api/common/config/CircuitBreakerFailurePredicate.java
@@ -2,12 +2,17 @@ package com.example.order_service.api.common.config;
 
 import com.example.order_service.api.common.exception.business.BusinessException;
 import com.example.order_service.api.common.exception.business.code.ErrorCode;
+import com.example.order_service.api.common.exception.external.ExternalClientException;
+import com.example.order_service.api.common.exception.external.ExternalServerException;
+import com.example.order_service.api.common.exception.external.ExternalSystemUnavailableException;
+import feign.RetryableException;
 import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 import java.util.function.Predicate;
 
+//TODO 삭제 공통 decoder 사용으로 마이그레이션
 public class CircuitBreakerFailurePredicate implements Predicate<Throwable> {
     @Override
     public boolean test(Throwable throwable) {
@@ -16,7 +21,16 @@ public class CircuitBreakerFailurePredicate implements Predicate<Throwable> {
             return errorCode.getStatus() >= 500;
         }
 
-        if (throwable instanceof IOException || throwable instanceof TimeoutException) {
+        if (throwable instanceof ExternalClientException) {
+            return false;
+        }
+        if (throwable instanceof ExternalServerException || throwable instanceof ExternalSystemUnavailableException) {
+            return true;
+        }
+
+        if (throwable instanceof IOException ||
+                throwable instanceof TimeoutException ||
+                throwable instanceof RetryableException) {
             return true;
         }
 
