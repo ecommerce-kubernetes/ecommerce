74918243fc51e6dee489f5f40aecbd8565b954cc
minsik2434 <minsik2434@gmail.com>
2026-06-09 05:41:10 +0900

    Test: 결제 도메인 테스트 작성
    Resolves: #422

---
+++ order-service/src/test/java/com/example/order_service/payment/domain/model/PaymentRecordTest.java
@@ -0,0 +1,42 @@
+package com.example.order_service.payment.domain.model;
+
+import com.example.order_service.common.domain.vo.Money;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PaymentRecordTest {
+
+    @Test
+    @DisplayName("결제 승인 레코드를 생성한다")
+    void createApproval(){
+        //given
+        Money amount = Money.wons(10000L);
+        PaymentMethod method = PaymentMethod.CARD;
+        LocalDateTime approvedAt = LocalDateTime.now();
+        //when
+        PaymentRecord approval = PaymentRecord.createApproval(amount, method, approvedAt);
+        //then
+        assertThat(approval)
+                .extracting("type", "amount", "method")
+                .containsExactlyInAnyOrder(TransactionType.PAYMENT, Money.wons(10000L), PaymentMethod.CARD);
+    }
+
+    @Test
+    @DisplayName("결제 취소 레코드를 생성한다")
+    void createCancellation(){
+        //given
+        Money amount = Money.wons(10000L);
+        PaymentMethod method = PaymentMethod.CARD;
+        LocalDateTime approvedAt = LocalDateTime.now();
+        //when
+        PaymentRecord cancellation = PaymentRecord.createCancellation(amount, method, approvedAt);
+        //then
+        assertThat(cancellation)
+                .extracting("type", "amount", "method")
+                .containsExactlyInAnyOrder(TransactionType.REFUND, Money.wons(10000L), PaymentMethod.CARD);
+    }
+}
