73ec249b19ddf893c5afe9c294c218d01889201a
minsik2434 <minsik2434@gmail.com>
2026-06-19 15:16:53 +0900

    Test: PaymentGateway 테스트 작성
    Resolves: #422

--- order-service/src/test/java/com/example/order_service/payment/external/PaymentGatewayTest.java
+++ order-service/src/test/java/com/example/order_service/payment/external/PaymentGatewayTest.java
@@ -2,6 +2,7 @@ package com.example.order_service.payment.external;
 
 import com.example.order_service.common.exception.business.BusinessException;
 import com.example.order_service.common.exception.external.ExternalClientException;
+import com.example.order_service.common.exception.external.ExternalNetworkTimeoutException;
 import com.example.order_service.common.exception.external.ExternalServerException;
 import com.example.order_service.common.exception.external.ExternalSystemUnavailableException;
 import com.example.order_service.infrastructure.adaptor.TossAdaptor;
@@ -11,6 +12,7 @@ import com.example.order_service.payment.application.external.dto.command.PGPaym
 import com.example.order_service.payment.application.external.dto.result.PgPaymentResult;
 import com.example.order_service.payment.application.external.mapper.PgMapper;
 import com.example.order_service.payment.exception.PaymentErrorCode;
+import org.instancio.Instancio;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -19,7 +21,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static com.example.order_service.support.TestFixtureUtil.fixtureMonkey;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.*;
@@ -43,9 +44,9 @@ public class PaymentGatewayTest {
         @DisplayName("결제를 승인한다")
         void confirm() {
             //given
-            PGPaymentCommand.Confirm command = fixtureMonkey.giveMeOne(PGPaymentCommand.Confirm.class);
-            TossClientResponse.Confirm response = fixtureMonkey.giveMeOne(TossClientResponse.Confirm.class);
-            PgPaymentResult.Approval approval = fixtureMonkey.giveMeOne(PgPaymentResult.Approval.class);
+            PGPaymentCommand.Confirm command = Instancio.create(PGPaymentCommand.Confirm.class);
+            TossClientResponse.Confirm response = Instancio.create(TossClientResponse.Confirm.class);
+            PgPaymentResult.Approval approval = Instancio.create(PgPaymentResult.Approval.class);
             given(adaptor.confirmPayment(anyString(), anyString(), anyLong())).willReturn(response);
             given(pgMapper.toResult(any(TossClientResponse.Confirm.class))).willReturn(approval);
             //when
@@ -58,7 +59,7 @@ public class PaymentGatewayTest {
         @DisplayName("결제 승인중 토스 서버에서 오류가 발생한 경우 비지니스 예외가 발생한다")
         void confirm_externalServerException() {
             //given
-            PGPaymentCommand.Confirm command = fixtureMonkey.giveMeOne(PGPaymentCommand.Confirm.class);
+            PGPaymentCommand.Confirm command = Instancio.create(PGPaymentCommand.Confirm.class);
             willThrow(new ExternalServerException("INTERNAL_SERVER_ERROR", "처리중 오류가 발생했습니다"))
                     .given(adaptor).confirmPayment(anyString(), anyString(), anyLong());
             //when
@@ -73,7 +74,7 @@ public class PaymentGatewayTest {
         @DisplayName("결제 승인중 토스 서버에서 클라이언트 에러가 발생한 경우 비지니스 예외가 발생한다")
         void confirm_externalClientException() {
             //given
-            PGPaymentCommand.Confirm command = fixtureMonkey.giveMeOne(PGPaymentCommand.Confirm.class);
+            PGPaymentCommand.Confirm command = Instancio.create(PGPaymentCommand.Confirm.class);
             willThrow(new ExternalClientException("TOSS_CLIENT_ERROR", "오류가 발생했습니다"))
                     .given(adaptor).confirmPayment(anyString(), anyString(), anyLong());
             //when
@@ -88,7 +89,7 @@ public class PaymentGatewayTest {
         @DisplayName("결제 승인중 토스 서버 접근 불가 에러가 발생한 경우 비지니스 예외가 발생한다")
         void confirm_unavailableServerException() {
             //given
-            PGPaymentCommand.Confirm command = fixtureMonkey.giveMeOne(PGPaymentCommand.Confirm.class);
+            PGPaymentCommand.Confirm command = Instancio.create(PGPaymentCommand.Confirm.class);
             willThrow(new ExternalSystemUnavailableException("UNAVAILABLE", "오류가 발생했습니다"))
                     .given(adaptor).confirmPayment(anyString(), anyString(), anyLong());
             //when
@@ -97,6 +98,100 @@ public class PaymentGatewayTest {
                     .isInstanceOf(BusinessException.class)
                     .extracting("errorCode")
                     .isEqualTo(PaymentErrorCode.PAYMENT_TOSS_UNAVAILABLE_ERROR);
+        }
+
+        @Test
+        @DisplayName("결제 승인중 타임아웃 에러가 발생한 경우 비지니스 예외가 발생한다")
+        void confirm_timeoutException() {
+            //given
+            PGPaymentCommand.Confirm command = Instancio.create(PGPaymentCommand.Confirm.class);
+            willThrow(new ExternalNetworkTimeoutException("TIMEOUT", "네트워크 응답 지연"))
+                    .given(adaptor).confirmPayment(anyString(), anyString(), anyLong());
+            //when
+            //then
+            assertThatThrownBy(() -> paymentGateway.confirm(command))
+                    .isInstanceOf(BusinessException.class)
+                    .extracting("errorCode")
+                    .isEqualTo(PaymentErrorCode.PAYMENT_TOSS_TIME_OUT_ERROR);
+        }
+    }
+
+    @Nested
+    @DisplayName("환불")
+    class Cancel {
+        @Test
+        @DisplayName("결제를 취소한다")
+        void cancel() {
+            //given
+            PGPaymentCommand.Cancel command = Instancio.create(PGPaymentCommand.Cancel.class);
+            PgPaymentResult.Cancellation result = Instancio.create(PgPaymentResult.Cancellation.class);
+            TossClientResponse.Cancel response = Instancio.create(TossClientResponse.Cancel.class);
+            given(adaptor.cancelPayment(anyString(), anyString(), anyLong())).willReturn(response);
+            given(pgMapper.toResult(any(TossClientResponse.Cancel.class))).willReturn(result);
+            //when
+            PgPaymentResult.Cancellation cancel = paymentGateway.cancel(command);
+            //then
+            assertThat(cancel).isEqualTo(result);
+        }
+
+        @Test
+        @DisplayName("결제 취소중 토스 서버에서 오류가 발생한 경우 비지니스 예외가 발생한다")
+        void cancel_externalServerException() {
+            //given
+            PGPaymentCommand.Cancel command = Instancio.create(PGPaymentCommand.Cancel.class);
+            willThrow(new ExternalServerException("INTERNAL_SERVER_ERROR", "처리중 오류가 발생했습니다"))
+                    .given(adaptor).cancelPayment(anyString(), anyString(), anyLong());
+            //when
+            //then
+            assertThatThrownBy(() -> paymentGateway.cancel(command))
+                    .isInstanceOf(BusinessException.class)
+                    .extracting("errorCode")
+                    .isEqualTo(PaymentErrorCode.PAYMENT_TOSS_SERVER_ERROR);
+        }
+
+        @Test
+        @DisplayName("결제 취소중 토스 서버에서 클라이언트 에러가 발생한 경우 비지니스 예외가 발생한다")
+        void cancel_externalClientException() {
+            //given
+            PGPaymentCommand.Cancel command = Instancio.create(PGPaymentCommand.Cancel.class);
+            willThrow(new ExternalClientException("TOSS_CLIENT_ERROR", "오류가 발생했습니다"))
+                    .given(adaptor).cancelPayment(anyString(), anyString(), anyLong());
+            //when
+            //then
+            assertThatThrownBy(() -> paymentGateway.cancel(command))
+                    .isInstanceOf(BusinessException.class)
+                    .extracting("errorCode")
+                    .isEqualTo(PaymentErrorCode.PAYMENT_TOSS_CLIENT_ERROR);
+        }
+
+        @Test
+        @DisplayName("결제 취소중 토스 서버 접근 불가 에러가 발생한 경우 비지니스 예외가 발생한다")
+        void cancel_unavailableServerException() {
+            //given
+            PGPaymentCommand.Cancel command = Instancio.create(PGPaymentCommand.Cancel.class);
+            willThrow(new ExternalSystemUnavailableException("UNAVAILABLE", "오류가 발생했습니다"))
+                    .given(adaptor).cancelPayment(anyString(), anyString(), anyLong());
+            //when
+            //then
+            assertThatThrownBy(() -> paymentGateway.cancel(command))
+                    .isInstanceOf(BusinessException.class)
+                    .extracting("errorCode")
+                    .isEqualTo(PaymentErrorCode.PAYMENT_TOSS_UNAVAILABLE_ERROR);
+        }
+
+        @Test
+        @DisplayName("결제 취소중 타임아웃 에러가 발생한 경우 비지니스 예외가 발생한다")
+        void cancel_timeoutException() {
+            //given
+            PGPaymentCommand.Cancel command = Instancio.create(PGPaymentCommand.Cancel.class);
+            willThrow(new ExternalNetworkTimeoutException("TIMEOUT", "네트워크 응답 지연"))
+                    .given(adaptor).cancelPayment(anyString(), anyString(), anyLong());
+            //when
+            //then
+            assertThatThrownBy(() -> paymentGateway.cancel(command))
+                    .isInstanceOf(BusinessException.class)
+                    .extracting("errorCode")
+                    .isEqualTo(PaymentErrorCode.PAYMENT_TOSS_TIME_OUT_ERROR);
         }
     }
 }
