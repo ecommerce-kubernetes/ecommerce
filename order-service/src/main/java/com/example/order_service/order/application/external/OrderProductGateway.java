e5c9ef54877c76580899058d1ea4f9a2837ec92e
minsik2434 <minsik2434@gmail.com>
2026-07-24 04:16:38 +0900

    Feat: 주문 상품 게이트웨이 로직 구현
    Resolves: #443

--- order-service/src/main/java/com/example/order_service/order/application/external/OrderProductGateway.java
+++ order-service/src/main/java/com/example/order_service/order/application/external/OrderProductGateway.java
@@ -1,37 +1,74 @@
 package com.example.order_service.order.application.external;
 
+import com.example.order_service.common.domain.vo.Money;
 import com.example.order_service.common.exception.external.ExternalCircuitBreakerException;
 import com.example.order_service.common.exception.external.ExternalClientException;
 import com.example.order_service.common.exception.external.ExternalServerException;
 import com.example.order_service.common.exception.external.ExternalSystemUnavailableException;
 import com.example.order_service.common.exception.gateway.DefaultGatewayException;
+import com.example.order_service.common.exception.gateway.ProductGatewayErrorCode;
 import com.example.order_service.infrastructure.adaptor.ProductAdaptor;
 import com.example.order_service.infrastructure.dto.command.ProductCommand;
 import com.example.order_service.infrastructure.dto.response.ProductClientResponse;
 import com.example.order_service.infrastructure.dto.response.product.ProductResponse;
 import com.example.order_service.order.application.external.dto.result.OrderProductResult;
+import com.example.order_service.order.application.external.dto.result.OrderProductStatus;
 import com.example.order_service.order.application.external.mapper.OrderProductMapper;
+import com.example.order_service.order.domain.vo.ProductOptionSnapshot;
+import com.example.order_service.order.domain.vo.ProductPriceSnapshot;
+import com.example.order_service.order.domain.vo.ProductSnapshot;
 import com.example.order_service.order.exception.OrderErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-/**
- * 주문 상품 도메인 통신을 담당하는 Gateway 서비스
- * <p>
- * 상품 도메인의 응답을 서비스 레이어의 Result로 매핑하여 반환
- * 상품 도메인 통신중 발생하는 예외를 비지니스 예외로 변환
- * </p>
- *
- * @author 최민식
- * @since 2026. 05. 22
- */
 @Service
 @RequiredArgsConstructor
 public class OrderProductGateway {
     private final ProductAdaptor productAdaptor;
     private final OrderProductMapper mapper;
+
+    public OrderProductResult getProducts(List<Long> productVariantIds) {
+        ProductResponse response = executeGetProducts(productVariantIds);
+        return mapToOrderProductResult(response);
+    }
+
+    private OrderProductResult mapToOrderProductResult(ProductResponse response) {
+        List<OrderProductResult.OrderProductDetail> orderProductDetails = mapToOrderProductDetail(response.products());
+        return OrderProductResult.builder()
+                .products(orderProductDetails)
+                .build();
+    }
+
+    private List<OrderProductResult.OrderProductDetail> mapToOrderProductDetail(List<ProductResponse.ProductDetail> products) {
+        return products.stream().map(product -> {
+            ProductSnapshot productSnapshot = ProductSnapshot.of(product.productId(), product.productVariantId(), product.sku(), product.productName(), product.thumbnail());
+            ProductPriceSnapshot priceSnapshot = ProductPriceSnapshot.of(Money.wons(product.unitPrice().originalPrice()),
+                    product.unitPrice().discountRate(), Money.wons(product.unitPrice().discountAmount()), Money.wons(product.unitPrice().discountedPrice()));
+            return OrderProductResult.OrderProductDetail.builder()
+                    .productSnapshot(productSnapshot)
+                    .status(mapToProductStatus(product.status()))
+                    .stock(product.stock())
+                    .priceSnapshot(priceSnapshot)
+                    .options(mapToOptions(product.options()))
+                    .build();
+        }).toList();
+    }
+
+    private OrderProductStatus mapToProductStatus(String productStatus) {
+        return switch (productStatus){
+            case "ON_SALE" -> OrderProductStatus.ON_SALE;
+            case "STOP_SALE" -> OrderProductStatus.STOP_SALE;
+            case "DELETED" -> OrderProductStatus.DELETED;
+            case "PREPARING" -> OrderProductStatus.PREPARING;
+            default -> throw new DefaultGatewayException(ProductGatewayErrorCode.PRODUCT_CLIENT_ERROR, "UNSUPPORTED_STATUS", "처리할 수 없는 상품 상태입니다");
+        };
+    }
+
+    private List<ProductOptionSnapshot> mapToOptions(List<ProductResponse.ProductOption> options) {
+        return options.stream().map(option -> ProductOptionSnapshot.of(option.optionTypeName(), option.optionValueName())).toList();
+    }
 
     @Deprecated
     public OrderProductResult getProductsDeprected(List<Long> variantIds) {
@@ -51,6 +88,20 @@ public class OrderProductGateway {
             throw new DefaultGatewayException(OrderErrorCode.ORDER_PRODUCT_UNAVAILABLE_SERVER_ERROR, e.getErrorCode(), e.getMessage());
         } catch (ExternalCircuitBreakerException e) {
             throw new DefaultGatewayException(OrderErrorCode.ORDER_PRODUCT_CIRCUIT_OPEN, e.getErrorCode(), e.getMessage());
+        }
+    }
+
+    private ProductResponse executeGetProducts(List<Long> productVariantIds) {
+        try {
+            return productAdaptor.getProducts(productVariantIds);
+        } catch (ExternalClientException e) {
+            throw new DefaultGatewayException(ProductGatewayErrorCode.PRODUCT_CLIENT_ERROR, e.getErrorCode(), e.getMessage());
+        } catch (ExternalServerException e) {
+            throw new DefaultGatewayException(ProductGatewayErrorCode.PRODUCT_SERVER_ERROR, e.getErrorCode(), e.getMessage());
+        } catch (ExternalSystemUnavailableException e) {
+            throw new DefaultGatewayException(ProductGatewayErrorCode.PRODUCT_UNAVAILABLE_SERVER_ERROR, e.getErrorCode(), e.getMessage());
+        } catch (ExternalCircuitBreakerException e) {
+            throw new DefaultGatewayException(ProductGatewayErrorCode.PRODUCT_CIRCUIT_OPEN, e.getErrorCode(), e.getMessage());
         }
     }
 }
--- order-service/src/test/java/com/example/order_service/order/application/external/OrderProductGatewayTest.java
+++ order-service/src/test/java/com/example/order_service/order/application/external/OrderProductGatewayTest.java
@@ -5,10 +5,12 @@ import com.example.order_service.common.exception.external.ExternalClientExcepti
 import com.example.order_service.common.exception.external.ExternalServerException;
 import com.example.order_service.common.exception.external.ExternalSystemUnavailableException;
 import com.example.order_service.common.exception.gateway.DefaultGatewayException;
+import com.example.order_service.common.exception.gateway.ProductGatewayErrorCode;
 import com.example.order_service.infrastructure.adaptor.ProductAdaptor;
 import com.example.order_service.infrastructure.dto.response.ProductClientResponse;
 import com.example.order_service.infrastructure.dto.response.product.ProductResponse;
 import com.example.order_service.order.application.external.dto.result.OrderProductResult;
+import com.example.order_service.order.application.external.dto.result.OrderProductStatus;
 import com.example.order_service.order.application.external.mapper.OrderProductMapper;
 import com.example.order_service.order.exception.OrderErrorCode;
 import org.instancio.Instancio;
@@ -45,104 +47,110 @@ public class OrderProductGatewayTest {
     void getProducts(){
         //given
         List<Long> variantIds = List.of(1L, 2L);
-        ProductResponse response = Instancio.of(ProductResponse.class)
-                .generate(field(ProductResponse::products), gen -> gen.collection().size(2))
-                .create();
+
+        ProductResponse.UnitPrice unitPrice = ProductResponse.UnitPrice.builder()
+                .originalPrice(10000L)
+                .discountRate(10)
+                .discountAmount(1000L)
+                .discountedPrice(9000L)
+                .build();
+
+        ProductResponse.ProductOption option = ProductResponse.ProductOption.builder()
+                .optionTypeName("사이즈")
+                .optionValueName("XL").build();
+        ProductResponse.ProductDetail detail = ProductResponse.ProductDetail.builder()
+                .productId(1L)
+                .productVariantId(1L)
+                .status("ON_SALE")
+                .stock(100)
+                .sku("SKU")
+                .productName("상품 이름")
+                .thumbnail("/product/product.jpg")
+                .unitPrice(unitPrice)
+                .options(List.of(option))
+                .build();
+        ProductResponse response = ProductResponse.builder().products(List.of(detail)).build();
         given(adaptor.getProducts(anyList())).willReturn(response);
         //when
+        OrderProductResult result = orderProductGateway.getProducts(variantIds);
         //then
-
+        assertThat(result.products()).hasSize(1);
+        OrderProductResult.OrderProductDetail mappedProduct = result.products().stream().findFirst().orElseThrow();
+        assertThat(mappedProduct.status()).isEqualTo(OrderProductStatus.ON_SALE);
+        assertThat(mappedProduct.productSnapshot().getProductId()).isEqualTo(1L);
+        assertThat(mappedProduct.priceSnapshot().getDiscountRate()).isEqualTo(10);
+        assertThat(mappedProduct.stock()).isEqualTo(100);
     }
 
-    @Nested
-    @DisplayName("상품 조회")
-    class GetProducts {
+    @Test
+    @DisplayName("상품 조회중 상품 서비스에서 서버 오류 발생시 예외로 변환된다")
+    void getProducts_ExternalServerException(){
+        //given
+        String code = "INTERNAL_SERVER_ERROR";
+        String message = "알 수 없는 에러가 발생했습니다";
+        List<Long> variantIds = List.of(1L, 2L);
+        willThrow(new ExternalServerException(code, message))
+                .given(adaptor).getProducts(any());
+        //when
+        //then
+        assertThatThrownBy(() -> orderProductGateway.getProducts(variantIds))
+                .isInstanceOf(DefaultGatewayException.class)
+                .hasMessage(String.format("Gateway Error: [%s] %s", code, message))
+                .extracting("errorCode", "externalErrorCode")
+                .containsExactly(ProductGatewayErrorCode.PRODUCT_SERVER_ERROR, code);
+    }
 
-        @Test
-        @DisplayName("상품을 조회한다")
-        void getProducts(){
-            //given
-            List<Long> variantIds = List.of(1L, 2L);
-            ProductClientResponse.ProductList productResponse = Instancio.create(ProductClientResponse.ProductList.class);
-            OrderProductResult productList = Instancio.create(OrderProductResult.class);
-            given(adaptor.getProductsDeprecated(any())).willReturn(productResponse);
-            given(productMapper.toResult(any(ProductClientResponse.ProductList.class))).willReturn(productList);
-            //when
-            OrderProductResult result = orderProductGateway.getProductsDeprected(variantIds);
-            //then
-            assertThat(result).isEqualTo(productList);
-        }
+    @Test
+    @DisplayName("상품 조회중 상품 서비스에서 클라이언트 오류 발생시 예외로 변환된다")
+    void getProducts_ExternalClientException(){
+        //given
+        String code = "INVALID_PRODUCT_REQUEST";
+        String message = "잘못된 상품 조회 요청입니다";
+        List<Long> variantIds = List.of(1L, 2L);
+        willThrow(new ExternalClientException(code, message))
+                .given(adaptor).getProducts(any());
+        //when
+        //then
+        assertThatThrownBy(() -> orderProductGateway.getProducts(variantIds))
+                .isInstanceOf(DefaultGatewayException.class)
+                .hasMessage(String.format("Gateway Error: [%s] %s", code, message))
+                .extracting("errorCode", "externalErrorCode")
+                .containsExactly(ProductGatewayErrorCode.PRODUCT_CLIENT_ERROR, code);
+    }
 
-        @Test
-        @DisplayName("상품 조회중 상품 서비스에서 서버 오류 발생시 예외로 변환된다")
-        void getProducts_ExternalServerException(){
-            //given
-            String code = "INTERNAL_SERVER_ERROR";
-            String message = "알 수 없는 에러가 발생했습니다";
-            List<Long> variantIds = List.of(1L, 2L);
-            willThrow(new ExternalServerException(code, message))
-                    .given(adaptor).getProductsDeprecated(any());
-            //when
-            //then
-            assertThatThrownBy(() -> orderProductGateway.getProductsDeprected(variantIds))
-                    .isInstanceOf(DefaultGatewayException.class)
-                    .hasMessage(String.format("Gateway Error: [%s] %s", code, message))
-                    .extracting("errorCode", "externalErrorCode")
-                    .containsExactly(OrderErrorCode.ORDER_PRODUCT_SERVER_ERROR, code);
-        }
+    @Test
+    @DisplayName("상품 조회중 상품 서비스에서 가용 불가 오류 발생시 예외로 변환된다")
+    void getProducts_ExternalSystemUnavailableException(){
+        //given
+        String code = "SERVICE_UNAVAILABLE";
+        String message = "상품 서비스 통신 장애";
+        List<Long> variantIds = List.of(1L, 2L);
+        willThrow(new ExternalSystemUnavailableException(code, message))
+                .given(adaptor).getProducts(any());
+        //when
+        //then
+        assertThatThrownBy(() -> orderProductGateway.getProducts(variantIds))
+                .isInstanceOf(DefaultGatewayException.class)
+                .hasMessage(String.format("Gateway Error: [%s] %s", code, message))
+                .extracting("errorCode", "externalErrorCode")
+                .containsExactly(ProductGatewayErrorCode.PRODUCT_UNAVAILABLE_SERVER_ERROR, code);
+    }
 
-        @Test
-        @DisplayName("상품 조회중 상품 서비스에서 클라이언트 오류 발생시 예외로 변환된다")
-        void getProducts_ExternalClientException(){
-            //given
-            String code = "INVALID_PRODUCT_REQUEST";
-            String message = "잘못된 상품 조회 요청입니다";
-            List<Long> variantIds = List.of(1L, 2L);
-            willThrow(new ExternalClientException(code, message))
-                    .given(adaptor).getProductsDeprecated(any());
-            //when
-            //then
-            assertThatThrownBy(() -> orderProductGateway.getProductsDeprected(variantIds))
-                    .isInstanceOf(DefaultGatewayException.class)
-                    .hasMessage(String.format("Gateway Error: [%s] %s", code, message))
-                    .extracting("errorCode", "externalErrorCode")
-                    .containsExactly(OrderErrorCode.ORDER_PRODUCT_CLIENT_ERROR, code);
-        }
-
-        @Test
-        @DisplayName("상품 조회중 상품 서비스에서 가용 불가 오류 발생시 예외로 변환된다")
-        void getProducts_ExternalSystemUnavailableException(){
-            //given
-            String code = "SERVICE_UNAVAILABLE";
-            String message = "상품 서비스 통신 장애";
-            List<Long> variantIds = List.of(1L, 2L);
-            willThrow(new ExternalSystemUnavailableException(code, message))
-                    .given(adaptor).getProductsDeprecated(any());
-            //when
-            //then
-            assertThatThrownBy(() -> orderProductGateway.getProductsDeprected(variantIds))
-                    .isInstanceOf(DefaultGatewayException.class)
-                    .hasMessage(String.format("Gateway Error: [%s] %s", code, message))
-                    .extracting("errorCode", "externalErrorCode")
-                    .containsExactly(OrderErrorCode.ORDER_PRODUCT_UNAVAILABLE_SERVER_ERROR, code);
-        }
-
-        @Test
-        @DisplayName("상품 조회중 상품 서비스 서킷 브레이커가 열린 경우 예외가 발생한다")
-        void getProducts_ExternalCircuitBreakerException(){
-            //given
-            String code = "PRODUCT_CIRCUIT_OPEN";
-            String message = "상품 서비스 서킷 오픈";
-            List<Long> variantIds = List.of(1L, 2L);
-            willThrow(new ExternalCircuitBreakerException(code, message))
-                    .given(adaptor).getProductsDeprecated(any());
-            //when
-            //then
-            assertThatThrownBy(() -> orderProductGateway.getProductsDeprected(variantIds))
-                    .isInstanceOf(DefaultGatewayException.class)
-                    .hasMessage(String.format("Gateway Error: [%s] %s", code, message))
-                    .extracting("errorCode", "externalErrorCode")
-                    .containsExactly(OrderErrorCode.ORDER_PRODUCT_CIRCUIT_OPEN, code);
-        }
+    @Test
+    @DisplayName("상품 조회중 상품 서비스 서킷 브레이커가 열린 경우 예외가 발생한다")
+    void getProducts_ExternalCircuitBreakerException(){
+        //given
+        String code = "PRODUCT_CIRCUIT_OPEN";
+        String message = "상품 서비스 서킷 오픈";
+        List<Long> variantIds = List.of(1L, 2L);
+        willThrow(new ExternalCircuitBreakerException(code, message))
+                .given(adaptor).getProducts(any());
+        //when
+        //then
+        assertThatThrownBy(() -> orderProductGateway.getProducts(variantIds))
+                .isInstanceOf(DefaultGatewayException.class)
+                .hasMessage(String.format("Gateway Error: [%s] %s", code, message))
+                .extracting("errorCode", "externalErrorCode")
+                .containsExactly(ProductGatewayErrorCode.PRODUCT_CIRCUIT_OPEN, code);
     }
 }
