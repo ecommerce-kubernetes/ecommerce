0fb53833b30cddaea31c4e3f32118bbba780ee90
minsik2434 <minsik2434@gmail.com>
2026-08-17 19:38:22 +0900

    Feat: 주문 사가 포인트 로직 구현
    Resolves: #443

--- order-service/src/main/java/com/example/order_service/saga/adapter/out/message/processor/PointMessageProcessor.java
+++ order-service/src/main/java/com/example/order_service/saga/adapter/out/message/processor/PointMessageProcessor.java
@@ -1,11 +1,29 @@
 package com.example.order_service.saga.adapter.out.message.processor;
 
+import com.example.order_service.saga.adapter.out.message.processor.dto.InventoryMessagePayload;
+import com.example.order_service.saga.adapter.out.message.processor.dto.PointMessagePayload;
+import com.example.order_service.saga.config.SagaTopicProperties;
+import com.example.order_service.saga.domain.event.ReduceInventoryEvent;
 import com.example.order_service.saga.domain.event.SagaEvent;
 import com.example.order_service.saga.domain.event.UsedPointEvent;
+import com.example.order_service.saga.exception.MessageSerializationException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class PointMessageProcessor implements SagaMessageProcessor{
+
+    private final ObjectMapper objectMapper;
+    private final SagaTopicProperties topic;
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
     @Override
     public boolean supports(SagaEvent event) {
         return event instanceof UsedPointEvent;
@@ -13,6 +31,33 @@ public class PointMessageProcessor implements SagaMessageProcessor{
 
     @Override
     public void process(SagaEvent event) {
+        if (event instanceof UsedPointEvent reduceEvent) {
+            PointMessagePayload payload = PointMessagePayload.from(reduceEvent);
+            sendKafkaMessage(event, payload, "UsedPointCommand");
+        }
+    }
 
+    private void sendKafkaMessage(SagaEvent event, PointMessagePayload payload, String eventType) {
+        String jsonPayload = toJson(payload);
+        Message<String> kafkaMessage = createKafkaMessage(event, jsonPayload, eventType);
+        kafkaTemplate.send(kafkaMessage);
+    }
+
+    private String toJson(PointMessagePayload payload) {
+        try {
+            return objectMapper.writeValueAsString(payload);
+        } catch (JsonProcessingException e) {
+            throw new MessageSerializationException("포인트 사가 메시지 직렬화 실패.");
+        }
+    }
+
+    private Message<String> createKafkaMessage(SagaEvent event, String payload, String eventType) {
+        return MessageBuilder
+                .withPayload(payload)
+                .setHeader(KafkaHeaders.TOPIC, topic.productSagaCommand())
+                .setHeader(KafkaHeaders.KEY, String.valueOf(event.orderId()))
+                .setHeader("X-Saga-Id", event.sagaId())
+                .setHeader("X-Event-Type", eventType)
+                .build();
     }
 }
