8112c6efef43f98e42dc85b08ca5a6ac152e9f1f
minsik2434 <minsik2434@gmail.com>
2026-09-09 02:37:11 +0900

    Feat: 옵션 타입, 값 생성 도메인 메서드 작성
    Resolves: #449

--- product-service/src/main/java/com/example/product_service/common/exception/OptionErrorCode.java
+++ product-service/src/main/java/com/example/product_service/common/exception/OptionErrorCode.java
@@ -6,11 +6,11 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum OptionErrorCode implements ErrorCode {
+    OPTION_VALUE_DUPLICATE_NAME(ErrorCategory.NOT_FOUND, "OPTION_VALUE_DUPLICATE_NAME", "동일한 옵션 값 이름을 설정할 수 없습니다"),
     OPTION_NOT_FOUND(ErrorCategory.NOT_FOUND, "OPTION_001", "옵션을 찾을 수 없습니다"),
     DUPLICATE_NAME(ErrorCategory.NOT_FOUND, "OPTION_002", "동일한 옵션 이름이 존재합니다"),
     OPTION_IN_PRODUCT_OPTION(ErrorCategory.BUSINESS_CONFLICT, "OPTION_003", "옵션이 상품에 속해있습니다"),
     OPTION_VALUE_NOT_FOUND(ErrorCategory.NOT_FOUND, "OPTION_004", "옵션 값을 찾을 수 없습니다"),
-    OPTION_VALUE_DUPLICATE_NAME(ErrorCategory.NOT_FOUND, "OPTION_005", "해당 옵션에 동일한 이름의 옵션 값이 있습니다"),
     OPTION_VALUE_IN_VARIANT(ErrorCategory.BUSINESS_CONFLICT, "OPTION_006", "해당 옵션값이 상품 변형에 속해있습니다");
     private final ErrorCategory category;
     private final String code;
--- product-service/src/main/java/com/example/product_service/option/domain/OptionType.java
+++ product-service/src/main/java/com/example/product_service/option/domain/OptionType.java
@@ -1,12 +1,17 @@
 package com.example.product_service.option.domain;
 
+import com.example.product_service.common.exception.BusinessException;
+import com.example.product_service.common.exception.OptionErrorCode;
 import com.example.product_service.option.domain.context.CreateOptionTypeContext;
+import com.example.product_service.option.domain.context.CreateOptionValueContext;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.util.Assert;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -31,6 +36,8 @@ public class OptionType {
     }
 
     public static OptionType create(CreateOptionTypeContext context) {
+        validateDuplicateValueName(context.valueContexts());
+
         OptionType optionType = OptionType.builder()
                 .id(context.id())
                 .name(context.name())
@@ -45,5 +52,15 @@ public class OptionType {
 
     private void addOptionValues(List<OptionValue> optionValues) {
         this.optionValues.addAll(optionValues);
+    }
+
+    private static void validateDuplicateValueName(List<CreateOptionValueContext> valueContexts) {
+        HashSet<String> nameSet = new HashSet<>();
+
+        boolean isDuplicateName = valueContexts.stream().anyMatch(context -> !nameSet.add(context.name()));
+
+        if (isDuplicateName) {
+            throw new BusinessException(OptionErrorCode.OPTION_VALUE_DUPLICATE_NAME);
+        }
     }
 }
